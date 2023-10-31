2d0fcf6f5023624f5db520dc05d25d7cbdecae68
Reifan1212 <riogit644@gmail.com>
2023-10-31 19:35:57 +0800

    Initial commit

---
+++ app/src/main/java/com/rio/recyclerviewrio/Detail.java
@@ -0,0 +1,24 @@
+package com.rio.recyclerviewrio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Detail extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        int photoResource = getIntent().getIntExtra("HERO_PHOTO", 0);
+        String name = getIntent().getStringExtra( "HERO_NAME");
+        String description = getIntent().getStringExtra( "HERO_DESCRIPTION");
+        ImageView imageView = findViewById(R.id.img_item_photo);
+        TextView nameTextView = findViewById(R.id.tv_item_name);
+        TextView descriptionTextView = findViewById(R.id.tv_item_description);
+        imageView.setImageResource(photoResource);
+        nameTextView.setText(name);
+        descriptionTextView.setText(description);
+    }
+}
